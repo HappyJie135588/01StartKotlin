51b64aec9d570eb3636a8b14b41bd83bd700bb41
HappyJie135588 <361125241@qq.com>
2019-01-21 16:25:46 +0800

    9.1,自定义控件,9.2自定义动画,9.3自定义通知补提

---
+++ app/src/main/java/com/example/kotlin/a01startkotlin/class09/DrawRoundActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin.a01startkotlin.class09
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlin.a01startkotlin.R
+
+class DrawRoundActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_draw_round)
+    }
+}
